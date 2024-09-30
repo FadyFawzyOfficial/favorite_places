713d9523950403714ea1149c755a7e60c2afe978
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2024-09-30 10:50:01 +0300

    4. Use riverpod placesProvider to provide places

--- lib/widgets/places_list_view.dart
+++ lib/widgets/places_list_view.dart
@@ -1,14 +1,17 @@
+import 'package:favorite_places/providers/places_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'place_list_tile.dart';
 
-class PlacesListView extends StatelessWidget {
+class PlacesListView extends ConsumerWidget {
   const PlacesListView({super.key});
 
   @override
-  Widget build(context) {
+  Widget build(context, ref) {
+    final places = ref.read(placesProvider);
     return ListView.builder(
-      itemCount: 10,
+      itemCount: places.length,
       itemBuilder: (context, index) => const PlaceListTile(),
     );
   }
