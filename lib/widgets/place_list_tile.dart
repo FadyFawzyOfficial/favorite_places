24b9d8b4ef9df339072aae14437b77b182fe66ac
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2024-09-30 11:58:28 +0300

    6. Build PlaceView to navigate to & display all place details

--- lib/widgets/place_list_tile.dart
+++ lib/widgets/place_list_tile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../models/place.dart';
+import '../views/place_view.dart';
 
 class PlaceListTile extends StatelessWidget {
   final Place place;
@@ -8,5 +9,13 @@ class PlaceListTile extends StatelessWidget {
   const PlaceListTile({super.key, required this.place});
 
   @override
-  Widget build(context) => ListTile(title: Text(place.title));
+  Widget build(context) {
+    return ListTile(
+      title: Text(place.title),
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => PlaceView(place: place)),
+      ),
+    );
+  }
 }
