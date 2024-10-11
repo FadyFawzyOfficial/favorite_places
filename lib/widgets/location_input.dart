795c6c36e09992ea596a0e3d768b55bd05233f66
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2024-10-11 20:34:53 +0300

    4. Build a method that use location plugin to get the user's current location

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 
 class LocationInput extends StatefulWidget {
   const LocationInput({super.key});
@@ -8,6 +9,9 @@ class LocationInput extends StatefulWidget {
 }
 
 class _LocationInputState extends State<LocationInput> {
+  LocationData? location;
+  var isGettingLocation = false;
+
   @override
   Widget build(context) {
     return Column(
@@ -21,10 +25,12 @@ class _LocationInputState extends State<LocationInput> {
               color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
             ),
           ),
-          child: const Text(
-            'No location chosen',
-            style: TextStyle(color: Colors.white),
-          ),
+          child: isGettingLocation
+              ? const CircularProgressIndicator()
+              : const Text(
+                  'No location chosen',
+                  style: TextStyle(color: Colors.white),
+                ),
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -32,7 +38,7 @@ class _LocationInputState extends State<LocationInput> {
             TextButton.icon(
               icon: const Icon(Icons.location_on_rounded),
               label: const Text('Get current location'),
-              onPressed: () {},
+              onPressed: getCurrentLocation,
             ),
             TextButton.icon(
               icon: const Icon(Icons.map_rounded),
@@ -43,5 +49,36 @@ class _LocationInputState extends State<LocationInput> {
         )
       ],
     );
+  }
+
+  void getCurrentLocation() async {
+    Location location = Location();
+
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+    LocationData locationData;
+
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return;
+      }
+    }
+
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    setState(() => isGettingLocation = true);
+    locationData = await location.getLocation();
+    setState(() => isGettingLocation = false);
+
+    print(locationData.latitude);
+    print(locationData.longitude);
   }
 }
