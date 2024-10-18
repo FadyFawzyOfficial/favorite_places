9489cf9ba9df8adeae72b5da7b3c1337c9e886e3
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2024-10-18 16:18:21 +0300

    4. Build MapView to view place on map

---
+++ lib/views/map_view.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+import '../models/place.dart';
+
+class MapView extends StatefulWidget {
+  final bool isPicking;
+  final PlaceLocation location;
+
+  const MapView({
+    super.key,
+    required this.isPicking,
+    this.location = const PlaceLocation(
+      latitude: 37.422,
+      longitude: -122.084,
+      address: '',
+    ),
+  });
+
+  @override
+  State<MapView> createState() => _MapViewState();
+}
+
+class _MapViewState extends State<MapView> {
+  @override
+  Widget build(BuildContext context) {
+    final latLng = LatLng(widget.location.latitude, widget.location.longitude);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.isPicking ? 'Pick your Location' : 'Your Location'),
+        actions: [
+          if (widget.isPicking)
+            IconButton(
+              icon: const Icon(Icons.save_rounded),
+              onPressed: () {},
+            ),
+        ],
+      ),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: latLng,
+          zoom: 16,
+        ),
+        markers: {
+          Marker(
+            markerId: const MarkerId('m1'),
+            position: latLng,
+          ),
+        },
+      ),
+    );
+  }
+}
