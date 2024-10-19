0361c30f47a4fa93458f2a337641305d024bb70b
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2024-10-19 18:03:11 +0300

    4. Split creating & opening database into separated function (database getter)

--- lib/providers/places_provider.dart
+++ lib/providers/places_provider.dart
@@ -11,13 +11,7 @@ const placesTable = 'user_places';
 class PlacesNotifier extends StateNotifier<List<Place>> {
   PlacesNotifier() : super(const []);
 
-  void addPlace({required Place place}) async {
-    final appDirectory = await getApplicationDocumentsDirectory();
-    final filename = basename(place.image.path);
-    final copiedImage =
-        await place.image.copy('${appDirectory.path}/$filename');
-    final newPlace = place.copyWith(image: copiedImage);
-
+  Future<Database> get database async {
     final dbPath = await getDatabasesPath();
     final db = await openDatabase(
       join(dbPath, dbName),
@@ -26,7 +20,19 @@ class PlacesNotifier extends StateNotifier<List<Place>> {
       version: 1,
     );
 
+    return db;
+  }
+
+  void addPlace({required Place place}) async {
+    final appDirectory = await getApplicationDocumentsDirectory();
+    final filename = basename(place.image.path);
+    final copiedImage =
+        await place.image.copy('${appDirectory.path}/$filename');
+    final newPlace = place.copyWith(image: copiedImage);
+
     print(newPlace.toMap());
+
+    final db = await database;
 
     db.insert(placesTable, newPlace.toMap());
 
